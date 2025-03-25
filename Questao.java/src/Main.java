02359d62f0a2164a946f70b44596297dbe2cdc69
joaocamargo1227 <joaocamargo1227@gmail.com>
2025-03-25 20:04:05 -0300

    Add files via upload

---
+++ Questao.java/src/Main.java
@@ -0,0 +1,312 @@
+import java.util.Scanner;
+
+class Trabalho1 {
+    public static void main(String[] args) {
+        System.out.println("Faculdade: Unifan.");
+        System.out.println("Aluno: Joao Vitor Alves Camargo.");
+        System.out.println("Professor: Brenno.");
+        System.out.println("          Quiz Futebol          ");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("01)Qual foi o jogador mais caro do futebol em 2017?");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] Kylian Mbappe");
+        System.out.println("[B] Cristiano Ronaldo");
+        System.out.println("[C] Neymar Junior");
+        System.out.println("[D] Lionel Messi");
+        System.out.println("[E] Mohamed Salah");
+
+        System.out.println("Digite sua resposta: ");
+        String resposta = scanner.nextLine();
+
+        if(resposta.equals("c")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("02)Qual o maior artilheiro das copas do mundo?");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] Ronaldo Fenômeno");
+        System.out.println("[B] Miroslav Klose");
+        System.out.println("[C] Lionel Messi");
+        System.out.println("[D] Pelé");
+        System.out.println("[E] Gerd Muller");
+
+        System.out.println("Digite sua resposta: ");
+        String resposta2 = scanner2.nextLine();
+
+        if(resposta2.equals("b")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner3 = new Scanner(System.in);
+        System.out.println("03)No ano de 2009, foi criado um novo premio (Puskas)" +
+                "Para os jogadores que fizeram o gol mais bonito naquela temporada. Qual jogador ganhou o premio naquele ano?");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] Cristiano Ronaldo");
+        System.out.println("[B] Lionel Messi");
+        System.out.println("[C] Kaká");
+        System.out.println("[D] Ronaldinho");
+        System.out.println("[E] Roberto Carlos");
+
+        System.out.println("Digite sua resposta: ");
+        String resposta3 = scanner3.nextLine();
+
+        if(resposta3.equals("a")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner4 = new Scanner(System.in);
+        System.out.println("04)quem é o maior artilheiro da seleção brasileira?");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] Neymar Junior");
+        System.out.println("[B] Pelé");
+        System.out.println("[C] Ronaldo Fenômeno");
+        System.out.println("[D] Rivaldo");
+        System.out.println("[E] Romário");
+
+        System.out.println("Digite sua resposta: ");
+        String resposta4 = scanner4.nextLine();
+
+        if(resposta4.equals("a")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner5 = new Scanner(System.in);
+        System.out.println("05)Uma das maiores lendas do esporte foi o Maradona, mas em qual clube ele é mais idolo?");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] Barcelona");
+        System.out.println("[B] Newell´s Old Boys");
+        System.out.println("[C] Boca Juniors");
+        System.out.println("[D] Sevilla");
+        System.out.println("[E] Napoli");
+
+        System.out.println("Digite sua resposta: ");
+        String resposta5 = scanner5.nextLine();
+
+        if(resposta5.equals("e")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner6 = new Scanner(System.in);
+        System.out.println("06)Qual o maior campeão da Champions League?");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] Liverpool");
+        System.out.println("[B] Milan");
+        System.out.println("[C] Barcelona");
+        System.out.println("[D] Real Madrid");
+        System.out.println("[E] Bayern de Munique");
+
+        System.out.println("Digite sua resposta: ");
+        String resposta6 = scanner6.nextLine();
+
+        if(resposta6.equals("d")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner7 = new Scanner(System.in);
+        System.out.println("07)Sem sombra de duvidas o Pelé e o rei do futebol, mas infelismente não é o maior artilheiro." +
+                "quem é o maior artilheiro da historia(contando somente gols oficiais)?");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] Messi");
+        System.out.println("[B] Cristiano Ronaldo");
+        System.out.println("[C] Romario");
+        System.out.println("[D] Ferenc Puskas");
+        System.out.println("[E] Josef Bican");
+
+        System.out.println("Digite sua resposta: ");
+        String resposta7 = scanner7.nextLine();
+
+        if(resposta7.equals("b")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner8 = new Scanner(System.in);
+        System.out.println("08)Só três jogaroes na história marcaram 5 gols em uma só partida de champions league, quais os três?");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] Messi, Cristiano Ronaldo, Mbappe");
+        System.out.println("[B] Halland, Messi, Soarez");
+        System.out.println("[C] Halland, Messi, Luiz Adriano");
+        System.out.println("[D] Messi, Luiz Adriano, Mbappe");
+        System.out.println("[E] Halland, Mbappe, Herry Kane");
+
+        System.out.println("Digite sua resposta: ");
+        String resposta8 = scanner8.nextLine();
+
+        if(resposta8.equals("c")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner9 = new Scanner(System.in);
+        System.out.println("09)Existe um jogador com a capacidade de fazer magia em capo, qual jogador e esse?");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] Zidane");
+        System.out.println("[B] Backham");
+        System.out.println("[C] Figo");
+        System.out.println("[D] Eto");
+        System.out.println("[E] Ronaldinho");
+
+        System.out.println("Digite sua resposta: ");
+        String resposta9 = scanner9.nextLine();
+
+        if(resposta9.equals("e")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner10 = new Scanner(System.in);
+        System.out.println("10)Qual time ganhou mais o campeonato ingles?");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] Manchester Und");
+        System.out.println("[B] Manchester City");
+        System.out.println("[C] Liverpool");
+        System.out.println("[D] Chelsea");
+        System.out.println("[E] Arsenal");
+
+        System.out.println("Digite sua resposta: ");
+        String resposta10 = scanner10.nextLine();
+
+        if(resposta10.equals("a")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner11 = new Scanner(System.in);
+        System.out.println("11)no ano de 2003/2004 dois times inusitados foram para a final da champions, quais os dois finalista?");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] Porto e Arsenal");
+        System.out.println("[B] shakhtar e Borussia");
+        System.out.println("[C] Bayern e Barcelona");
+        System.out.println("[D] Porto e Monaco");
+        System.out.println("[E] Porto e Real");
+
+        System.out.println("Digite sua resposta: ");
+        String resposta11 = scanner11.nextLine();
+
+        if(resposta11.equals("d")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner12 = new Scanner(System.in);
+        System.out.println("12)Qual Time ganhou 6 titulos(sextete) em 2009?");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] Barcelona");
+        System.out.println("[B] Inter de Milião");
+        System.out.println("[C] Real Madrid");
+        System.out.println("[D] Liverpool");
+        System.out.println("[E] Manchester Und");
+
+        System.out.println("Digite sua resposta: ");
+        String resposta12 = scanner12.nextLine();
+
+        if(resposta12.equals("a")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner13 = new Scanner(System.in);
+        System.out.println("13)Qual o pais com mais copas do mundo?");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] França");
+        System.out.println("[B] Alemanha");
+        System.out.println("[C] Argentina");
+        System.out.println("[D] Brasil");
+        System.out.println("[E] Italia");
+
+        System.out.println("Digite sua resposta: ");
+        String resposta13 = scanner13.nextLine();
+
+        if(resposta13.equals("d")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner14 = new Scanner(System.in);
+        System.out.println("14)Onde foi disputada a ultima copa do mundo?");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] Russia");
+        System.out.println("[B] Estados Unidos");
+        System.out.println("[C] Qatar");
+        System.out.println("[D] Japão");
+        System.out.println("[E] Espanha");
+
+        System.out.println("Digite sua resposta: ");
+        String resposta14 = scanner14.nextLine();
+
+        if(resposta14.equals("c")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+
+        Scanner scanner15 = new Scanner(System.in);
+        System.out.println("15)Onde será a proxima copa do mundo?");
+
+        System.out.println("Alternativa");
+
+        System.out.println("[A] America do norte");
+        System.out.println("[B] argentina, Brasil e equador");
+        System.out.println("[C] Espanha, França e Inglaterra");
+        System.out.println("[D] Japão, Coreia do Norte e Índia");
+        System.out.println("[E] Australia, Irlanda do Norte e Noruega");
+
+        System.out.println("Digite sua resposta: ");
+        String resposta15 = scanner15.nextLine();
+
+        if(resposta15.equals("a")){
+            System.out.println("Resposta correta!");
+        } else{
+            System.out.println("Resposta errada!");
+        }
+    }
+}
